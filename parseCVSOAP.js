3c5e4106351bef812f8f92df435022a82a48f727
Leo Kodish <lkkodish@gmail.com>
2018-08-05 16:52:06 +0300

    uncomment SOAP request

--- parseCVSOAP.js
+++ parseCVSOAP.js
@@ -1,20 +1,20 @@
 var soap = require('soap');
 var fs = require('fs');
 
-// var url = 'http://processing.resumeparser.com/ParsingTools.soap?wsdl';
-// var args = {
-// 	product_code: '06cb79cec44eff39d7e694f443c890f0',
-//   	document_title: 'test_resume',
-//   	document_data: fs.readFileSync('attachment.txt', "utf8")
-// };
-// soap.createClient(url, function(err, client) {
-//   client.ParseDocNew(args, function(err, result) {
-//       fs.writeFile('resumeOutput.json', result.return.$value, function(err) {
-//       	if (err) console.log("Error writing resume data to file: " + err);
-//       	console.log("Successfully wrote resume data to file!");
-//       })
-//   });
-// });
+var url = 'http://processing.resumeparser.com/ParsingTools.soap?wsdl';
+var args = {
+	product_code: '06cb79cec44eff39d7e694f443c890f0',
+  	document_title: 'test_resume',
+  	document_data: fs.readFileSync('attachment.txt', "utf8")
+};
+soap.createClient(url, function(err, client) {
+  client.ParseDocNew(args, function(err, result) {
+      fs.writeFile('resumeOutput.json', result.return.$value, function(err) {
+      	if (err) console.log("Error writing resume data to file: " + err);
+      	console.log("Successfully wrote resume data to file!");
+      })
+  });
+});
 
 
 fs.readFile('resumeOutput.json', function(err, body) {
